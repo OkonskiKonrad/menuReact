ae89a1ea3f84d643dbb7d271ab74836174b868f5
Konrad <okonskikonrad94@gmail.com>
2023-05-29 11:19:03 +0200

    dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,41 @@
+import {useEffect, useState} from "react";
+import menu from "./data.js";
+import Categories from "./Categories.jsx";
+import Menu from "./Menu.jsx";
+import Title from "./Title.jsx";
+
+
+// const tempCategories = menu.map((item)=> item.category);
+// const tempSet = new Set(tempCategories)
+// const tempItems = ["all",...tempSet]
+// console.log(tempItems);
+
+//short version
+
+const allCategories = ['all', ...new Set(menu.map((item) => item.category))]
+console.log(allCategories)
+
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+    const [menuItems, setMenuItems] = useState(menu);
+    const [categories, setCategories] = useState(allCategories);
+    const filterItems = (category) => {
+        if (category === 'all') {
+            setMenuItems(menu)
+            return;
+        }
+        const newItems = menu.filter((item) => item.category === category)
+        setMenuItems(newItems);
+    }
+
+    return (
+        <main>
+            <section className="menu">
+                <Title text='our menu'/>
+                <Categories categories={categories} filterItems={filterItems}/>
+                <Menu items={menuItems}/>
+            </section>
+        </main>
+    );
 };
 export default App;
